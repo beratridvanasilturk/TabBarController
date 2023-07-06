7ed80a045c2a49cc0e8376cf4ab22f051d309e2d
beratridvanasilturk <br.asltrk@gmail.com>
2023-07-06 16:16:39 +0300

    Added 1 More View Con. to Original Tab Bar Con. from Relationship Segue

---
+++ Tab Bar Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tab Bar Controller
+//
+//  Created by Berat Rıdvan Asiltürk on 6.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
